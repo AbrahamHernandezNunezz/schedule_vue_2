69057f4ad44e8f1fc5b7fa2c1089fb741247096e
AbrahamHernandezNunezz <abraham.hernandez.nunezz@gmail.com>
2022-03-03 23:59:20 -0600

    component new item

--- src/components/contacts/ContactList.vue
+++ src/components/contacts/ContactList.vue
@@ -20,50 +20,7 @@ export default {
       { text: "Phone", value: "phone" },
       { text: "Favorite", value: "isFavorite" }
     ],
-    items: [
-      {
-        id: 1,
-        name: "bolo",
-        email: "bollo@gmial.com",
-        phone: 545234324,
-        isFavorite: false,
-      },
-      {
-        id: 2,
-        name: "bollo",
-        email: "bollo@gmial.com",
-        phone: 5451234324,
-        isFavorite: false,
-      },
-      {
-        id: 3,
-        name: "bo",
-        email: "bollo@gmial.com",
-        phone: 545233234,
-        isFavorite: false,
-      },
-      {
-        id: 4,
-        name: "Gordolobo",
-        email: "bollo@gmial.com",
-        phone: 2345234324,
-        isFavorite: false,
-      },
-      {
-        id: 5,
-        name: "bolasdo",
-        email: "bollo@gmial.com",
-        phone: 145234324,
-        isFavorite: false,
-      },
-      {
-        id: 6,
-        name: "bolas",
-        email: "bollo@gmial.com",
-        phone: 125234324,
-        isFavorite: false,
-      },
-    ],
   }),
+  props: ["items"]
 };
 </script>
